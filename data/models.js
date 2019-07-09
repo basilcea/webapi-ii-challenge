078ade9841c69aae7575e70536db171c55fd86fe
basilcea <ogbonna.basil3@gmmail.com>
2019-07-09 15:16:09 +0100

    implement get by id - implement delete by id

--- data/models.js
+++ data/models.js
@@ -1,26 +1,77 @@
-const posts = require('./db');
+const posts = require("./db");
 
-const getAllPosts  = async (req,res) => {
-    try{
-        const data = await posts.find()
-        return res.status(200).json({
-            status:200,
-            data
-        })
-    }catch(err){
-        return res.status(500).json({
-            status:500,
-            error: "The posts information could not be retrieved." 
-        })
+const getAllPosts = async (req, res) => {
+  try {
+    const data = await posts.find();
+    return res.status(200).json({
+      status: 200,
+      data
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      error: "The posts information could not be retrieved."
+    });
+  }
+};
+
+const getPost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await posts.findById(id);
+    if (!data) {
+      return res.status(404).json({
+        status: 404,
+        message: "The post with the specified ID does not exist."
+      });
     }
-} 
-const createPost = async (req,res) => {
+    return res.status(200).json({
+      status: 200,
+      data
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      error: "The post information could not be retrieved."
+    });
+  }
+};
 
-}  
-const createComment  = async (req,res) => {}  
-const getPost  = async (req,res) => {}  
-const getPostComments = async (req,res) => {}  
-const editPost = async (req,res) => {}  
-const deletePost = async (req,res) => {}  
+const deletePost = async (req, res) => {
+    const {id} = req.params
+    try {
+        const data = await posts.remove(id);
+        if (!data) {
+          return res.status(404).json({
+            status: 404,
+            message: "The post with the specified ID does not exist."
+          });
+        }
+        return res.status(200).json({
+          status: 200,
+          message:'Deleted successfully'
+        });
+      } catch (err) {
+        return res.status(500).json({
+          status: 500,
+          error: "The post could not be removed"
+        });
+      }
 
-module.exports ={createPost , createComment , getAllPosts , getPost , getPostComments , editPost , deletePost}
+};
+const createPost = async (req, res) => {};
+const createComment = async (req, res) => {};
+
+const getPostComments = async (req, res) => {};
+const editPost = async (req, res) => {};
+
+
+module.exports = {
+  createPost,
+  createComment,
+  getAllPosts,
+  getPost,
+  getPostComments,
+  editPost,
+  deletePost
+};
--- data/route.js
+++ data/route.js
@@ -5,9 +5,9 @@ const postModels = require("./models");
 // router.post("/", postModels.createPost);
 // router.post("./id/comments",postModels.createComment);
 router.get("/", postModels.getAllPosts);
-// router.get("/:id", postModels.getPost);
+router.get("/:id", postModels.getPost);
 // router.get("/:id/comments", postModels.getPostComments);
 // router.put("/:id", postModels.editPost);
-// router.delete("/:id", postModels.deletePost);
+router.delete("/:id", postModels.deletePost);
 
 module.exports = router;
