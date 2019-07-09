6177c33684ff41b91910e6ff095faf5daef88fc3
basilcea <ogbonna.basil3@gmmail.com>
2019-07-09 17:37:29 +0100

    implement crud endpoints for post api

--- data/route.js
+++ data/route.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const postModels = require("./models");
 
-// router.post("/", postModels.createPost);
-// router.post("./id/comments",postModels.createComment);
+router.post("/", postModels.createPost);
+router.post("/:id/comments",postModels.createComment);
 router.get("/", postModels.getAllPosts);
 router.get("/:id", postModels.getPost);
-// router.get("/:id/comments", postModels.getPostComments);
-// router.put("/:id", postModels.editPost);
+router.get("/:id/comments", postModels.getPostComments);
+router.put("/:id", postModels.editPost);
 router.delete("/:id", postModels.deletePost);
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const router = require('./data/route')
 const server = express()
 
 server.use(express.json());
-server.use('/api/posts', router);
 server.use(express.urlencoded({ extended:true }))
-
-
+server.use('/api/posts', router);
 server.listen(5000, ()=>{
     console.log('Server running on 5000...')
 })
